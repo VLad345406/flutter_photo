214818f027d290d0a28ccdcf5e4f878f4ad2f2a1
Vladyslav Permiakov <vlad.piermiakov@gmail.com>
2024-05-18 22:53:36 +0300

    Fix input username after google_sigh_in (now user can`t repeat nickname)

--- lib/services/authentication/google_sigh_in_add_info_firestore.dart
+++ lib/services/authentication/google_sigh_in_add_info_firestore.dart
@@ -3,32 +3,37 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_qualification_work/screens/main/main_screen.dart';
 import 'package:flutter_qualification_work/services/snack_bar_service.dart';
+import 'package:flutter_qualification_work/services/validate_username_service.dart';
 
 Future googleSighInAddInfoFirestore(
     BuildContext context, String nickName) async {
   if (nickName.isEmpty) {
     snackBar(context, 'Input nick name!');
   } else {
-    String? userEmail = FirebaseAuth.instance.currentUser?.email;
-    String? userID = FirebaseAuth.instance.currentUser?.uid;
-    try {
-      final FirebaseFirestore fireStore = FirebaseFirestore.instance;
-      fireStore.collection('users').doc(userID).set({
-        'uid': userID,
-        'email': userEmail,
-        'user_name': nickName,
-        'avatar_link': '',
-        'count_image' : 0,
-        'name' : '',
-      }, SetOptions(merge: true));
-      Navigator.pushAndRemoveUntil(context,
-          MaterialPageRoute(builder: (context) {
-        return MainScreen();
-      }), (route) => false);
-    } on FirebaseAuthException catch (e) {
-      snackBar(context, e.message.toString());
-    } catch (e) {
-      snackBar(context, e.toString());
+    if (!await validateUsername(nickName)) {
+      String? userEmail = FirebaseAuth.instance.currentUser?.email;
+      String? userID = FirebaseAuth.instance.currentUser?.uid;
+      try {
+        final FirebaseFirestore fireStore = FirebaseFirestore.instance;
+        fireStore.collection('users').doc(userID).set({
+          'uid': userID,
+          'email': userEmail,
+          'user_name': nickName,
+          'avatar_link': '',
+          'count_image': 0,
+          'name': '',
+        }, SetOptions(merge: true));
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (context) {
+              return MainScreen();
+            }), (route) => false);
+      } on FirebaseAuthException catch (e) {
+        snackBar(context, e.message.toString());
+      } catch (e) {
+        snackBar(context, e.toString());
+      }
+    } else {
+      snackBar(context, 'That nick name is already exist!');
     }
   }
 }
